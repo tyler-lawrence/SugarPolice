fd236802d68b0b6749d19f68ecc2233d62a47828
Tyler Lawrence <tyler.lawrence15@gmail.com>
2023-09-05 21:14:57 -0400

    shortcut works. need to figure out how to get the entered prompt to save to user defaults

---
+++ SoundTheAlarmIntent/IntentHandler.swift
@@ -0,0 +1,66 @@
+//
+//  IntentHandler.swift
+//  SoundTheAlarmIntent
+//
+//  Created by Tyler Lawrence1 on 9/5/23.
+//
+
+import Intents
+
+// As an example, this class is set up to handle Message intents.
+// You will want to replace this or add other intents as appropriate.
+// The intents you wish to handle must be declared in the extension's Info.plist.
+
+// You can test your example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+// "<myApp> John saying hello"
+// "Search for messages in <myApp>"
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        guard intent is SoundTheAlarmIntent else {
+          fatalError("Unhandled Intent error : \(intent)")
+        }
+       return SoundTheAlarmHandler()
+    }
+
+}
+
+class SoundTheAlarmHandler: NSObject, SoundTheAlarmIntentHandling {
+    
+    func handle(intent: SoundTheAlarmIntent, completion: @escaping (SoundTheAlarmIntentResponse) -> Void) {
+       if let crime = intent.crime {
+           addInfraction(crime: crime)
+           completion(SoundTheAlarmIntentResponse.success(crime: crime))
+       }
+    }
+    
+    func resolveCrime(for intent: SoundTheAlarmIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
+        guard let title = intent.crime else {
+           completion(INStringResolutionResult.needsValue())
+           return
+        }
+        completion(INStringResolutionResult.success(with: title))
+    }
+    
+    func addInfraction(crime: String) {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(crime, forKey: "message")
+    }
+    
+    private func donateIntent() {
+        let intent = SoundTheAlarmIntent()
+        intent.suggestedInvocationPhrase = "We have an infraction"
+        let interaction = INInteraction(intent: intent, response: nil)
+        interaction.donate { (error) in
+            if error != nil {
+                if let error = error as NSError? {
+                    print("Interaction donation failed: \(error.description)")
+                } else {
+                    print("Successfully donated interaction")
+                }
+            }
+        }
+    }
+}
